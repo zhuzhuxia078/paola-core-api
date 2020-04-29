8c0a2384e315580dde7ff23295808de447eba219
Anthony Pecchillo <anthonypecchillo@gmail.com>
2020-04-29 16:00:07 +0200

    Update Ping Sequelize Model

--- database/models/Ping.js
+++ database/models/Ping.js
@@ -1,15 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
   const Ping = sequelize.define('ping', {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
+      field: 'id',
     },
     message: {
+      type: DataTypes.STRING(255),
       allowNull: false,
-      // field: 'room_id',
-      type: DataTypes.STRING,
+      field: 'message',
     },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: 'updated_at',
+    },
+  }, {
+    tableName: 'pings',
   });
 
   return Ping;
