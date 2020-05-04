1d4557ccc61b9fb2a8e45c55de1b352f24dbd88f
Anthony Pecchillo <anthonypecchillo@gmail.com>
2020-05-05 01:12:32 +0200

    Add /students endpoint to apiRouter

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,10 +1,17 @@
 const apiRouter = require('express').Router();
 const { pings } = require('../database/controllers/ping');
+const { students } = require('../database/controllers/students');
 
 apiRouter.get('/ping', pings.get);
 apiRouter.post('/ping', pings.post);
 apiRouter.put('/ping', pings.put);
 apiRouter.patch('/ping', pings.patch);
 apiRouter.delete('/ping', pings.delete);
+
+apiRouter.get('/students', students.get);
+apiRouter.post('/students', students.post);
+apiRouter.put('/students', students.put);
+apiRouter.patch('/students', students.patch);
+apiRouter.delete('/students', students.delete);
 
 module.exports = apiRouter;
